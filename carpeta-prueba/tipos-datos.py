b6baac86dd2952ae8120c9a40e9ab49b648c6039
César Vargas <cesarvargasescorcia@gmail.com>
2023-09-28 12:37:39 -0600

    Tipos datos: string, int, float, bool

---
+++ carpeta-prueba/tipos-datos.py
@@ -0,0 +1,12 @@
+x = 10
+y: float = 10.5
+saludo: str = "Hola!"
+valor_bool = True
+print(x)
+print(y)
+print(saludo)
+print(valor_bool)
+print("X tipo dato: ", type(x))
+print("Saludo tipo dato: ", type(saludo))
+print("y tipo dato: ", type(y))
+print("Booleado tipo dato: ", type(valor_bool))
