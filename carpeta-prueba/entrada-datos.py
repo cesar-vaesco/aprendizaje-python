0f483de5b36f82a7149d19d042d83da3f2e8dff4
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-02 17:05:58 -0600

    Casteo a datos númericos ingresados por consola

--- carpeta-prueba/entrada-datos.py
+++ carpeta-prueba/entrada-datos.py
@@ -1,5 +1,21 @@
 entrada = input("Ingresa un valor: ")
 print("El valor ingresado fue:", entrada)
-print("\tFin del programa...")
+print("\n\tContinua el programa...")
 
+valor = input("Ingresa un valor númerico: \n")
 
+print("Valor ingresado:", valor)
+print("Tipo de dato de la variable valor: ", type(valor))
+print("Casteo de la variable valor....")
+valor = float(valor)
+print("Valor ingresado:", valor)
+print("Tipo de dato de la variable valor: ", type(valor))
+
+print("\tSuma de valores...")
+numero_1=input("Ingresa 1er valor: ")
+numero_2=input("Ingresa 2do valor: ")
+
+resultado = int(numero_1) + int(numero_2)
+
+print("Resultado:", resultado)
+print("\n\tFin del programa...")
