0e764efb942afe6f63feb58203f35511d562b3d2
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-03 11:54:22 -0600

    Operadores: operadores de comparación

---
+++ operadores/ope-comparacion.py
@@ -0,0 +1,20 @@
+a = int(input("Ingrese primer valor...."))
+b = int(input("Ingrese segundo valor5 valor...."))
+
+resultado = a == b
+print(f"{a} es igual(==) a {b}? {resultado}")
+
+resultado = a != b
+print(f"{a} es diferente(!) de {b}? {resultado}")
+
+resultado = a > b
+print(f"{a} es mayor(>) de {b}? {resultado}")
+
+resultado = a < b
+print(f"{a} es menor(<) de {b}? {resultado}")
+
+resultado = a >= b
+print(f"{a} es mayor(>) o igual(=) de {b}? {resultado}")
+
+resultado = a <= b
+print(f"{a} es menor(<) o igual (=) de {b}? {resultado}")
