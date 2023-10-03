498f305ecd50061b634eaefe2d4b0f2f64a292cb
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-03 11:26:12 -0600

    Operadores: operadores de asignación

---
+++ operadores/ope-asignacion.py
@@ -0,0 +1,31 @@
+var = 10
+
+print(f"{var}")
+print(f"Incrementar valor con operador de asignación. Valor inicial de la variable: {var}")
+
+var = var + 1
+print(f"{var}")
+var += 1
+print(f"{var}")
+
+print(f"Restar valor con operador de asignación. Valor inicial de la variable: {var}")
+
+var = var - 1
+print(f"{var}")
+var -= 1
+print(f"{var}")
+
+
+print(f"Multiplicar  * 3 valor con operador de asignación. Valor inicial de la variable: {var}")
+var = var * 3
+print(f"{var}")
+print(f"Multiplicar  * 3 valor con operador de asignación. Valor inicial de la variable: {var}")
+var *= 3
+print(f"{var}")
+
+print(f"Dividir entre 3 valor con operador de asignación. Valor inicial de la variable: {var}")
+var = var / 3
+print(f"{var}")
+print(f"Dividir / 3 valor con operador de asignación. Valor inicial de la variable: {var}")
+var /= 3
+print(f"{var}")
