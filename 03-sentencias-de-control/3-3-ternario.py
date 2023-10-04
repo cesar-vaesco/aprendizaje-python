459afd6930f855591b61b2f4fb177359c8b16975
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-03 19:58:32 -0600

    Sent-control: ternario-if-else

---
+++ 03-sentencias-de-control/3-3-ternario.py
@@ -0,0 +1,12 @@
+condicion = False
+
+
+# if condicion:
+#     print("Condición verdadera")
+# else:
+#     print("Condición falsa")    
+# crlt k ctrl c
+
+    
+
+print("Condición verdadera") if condicion else print("Condición falsa")
