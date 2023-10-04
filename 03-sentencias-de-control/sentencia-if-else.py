de81df3dd03e81ef550ece69f9a4f7825d7dde20
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-03 19:33:45 -0600

    Sent-control:if-else

---
+++ 03-sentencias-de-control/sentencia-if-else.py
@@ -0,0 +1,12 @@
+# Una cadena vacía entrega un valor False
+# condicion = ''
+
+condicion = 123
+
+
+if condicion == True:
+    print("La condición es verdadera")
+elif condicion == False:
+    print("La condición es falsa")
+else:
+    print("La condición no es reconocida")
