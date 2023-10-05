cf806bf100a6520570850418cb070bb1d3b75953
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-05 12:23:02 -0600

    Ciclos: while-ejer2

---
+++ 04-ciclos/4-3-ciclo-while-ejer2.py
@@ -0,0 +1,10 @@
+print("\t Imprimir números del 5 al 1 con ciclo while de forma descendente")
+
+minimo = 1
+contador = 5
+
+while contador >= minimo:
+    print(contador)
+    contador -= 1
+else:
+    print("\tFin del ciclo")
