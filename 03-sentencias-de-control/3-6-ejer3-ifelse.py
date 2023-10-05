962fb74f671c30b89479f23ca6e3b48e6bec0597
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-05 11:36:43 -0600

    Sent-control: ejercicio 3 if-else

---
+++ 03-sentencias-de-control/3-6-ejer3-ifelse.py
@@ -0,0 +1,19 @@
+calificacion = int(input("Ingrese su calificación\n\t"))
+
+calificacion_final = ""
+
+if calificacion == 10:
+    calificacion_final = "A"
+elif calificacion <=9  and calificacion >= 8:
+    calificacion_final = "B"
+elif calificacion <= 8  and calificacion >= 7:
+    calificacion_final = "C"
+elif calificacion <= 7 and calificacion >= 6:
+    calificacion_final = "D"
+elif calificacion > 0 and calificacion < 6:
+    calificacion_final = "F"
+else:
+    print("Error de asignación - Valor incorrecto")
+
+
+print(f"Valor asignado: {calificacion} - Calificación: {calificacion_final}")
