10c6c3f2f1ac1b7ae07554ffec22a43c831b389c
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-05 12:17:43 -0600

    Ciclos: while-ejer1

---
+++ 04-ciclos/4-2-ciclo-while-ejer1.py
@@ -0,0 +1,9 @@
+print("\t Imprimir números del 0 al 5 con ciclo while")
+
+contador = 0
+maximo = 5
+while contador <= maximo:
+    print(contador)
+    contador += 1
+else:
+    print("\tFin del ciclo")
