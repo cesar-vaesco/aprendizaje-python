9559bd7b0fd83fb7e64f51517a97f47c85582334
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-05 12:11:43 -0600

    Ciclos: ciclo while

---
+++ 04-ciclos/4-1-ciclo-while.py
@@ -0,0 +1,23 @@
+"""
+while (condicion):
+        cierto se ejecuta bloque de código
+ 
+Si la condición cambia a falso se termina la ejecución        
+"""
+
+
+# condicion = True
+
+# while condicion:
+#     print("Ejecutando el ciclo While")
+#     condicion = False
+# else:
+#     print("Fin del ciclo While")
+
+contador = 0
+
+while contador < 3:
+    print("\t",contador)
+    contador += 1
+else:
+    print("\t\nFin de ciclo while")
