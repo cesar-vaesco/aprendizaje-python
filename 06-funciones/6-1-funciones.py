64d86283340a99e9f5b7e6c07f18abb05215a392
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-09 08:08:57 -0600

    Funciones: funciones y paso de parametros

---
+++ 06-funciones/6-1-funciones.py
@@ -0,0 +1,18 @@
+# Definición - declaración de funciones
+
+
+def mi_funcion():
+    print("\nSaludos desde mi función...")
+
+
+def saludo(nombre, apellido):
+    print(f"\nHola")
+    print(f"\nBienvenid@ {nombre} {apellido}\n")
+
+
+# Fin de definición - declaración de funciones
+
+
+mi_funcion()
+saludo("César", "Vargas")
+saludo("Vanessa", "Vargas Cortez")
