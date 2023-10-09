3fbf67fd2287e6ec76965c3755ffa14116efa448
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-09 17:28:54 -0600

    Clases y objetos: robusteciendo método init

--- 07-clases_y_objetos/Persona.py
+++ 07-clases_y_objetos/Persona.py
@@ -6,22 +6,26 @@ es inicializar los atributos del objeto que creamos.
 
 class Persona:
     # Atribuitos de clase
-    def __init__(self, _nombre, _apellido, _edad):
+    def __init__(self, _nombre, _apellido, _edad, *_valores, **_terminos):
         self.nombre = _nombre
         self.apellido = _apellido
         self.edad = _edad
+        self.valores = _valores
+        self.terminos = _terminos
 
     # la palabra self pasa la referencia de los valores asignados a los atributos de la clase
     def mostrar_detalle(self):
         return f"""
 - Nombre: {self.nombre} 
 - Apellido: {self.apellido} 
-- Edad: {self.edad} 
+- Edad: {self.edad}
+- Calle: {self.valores}
+- Frutas: {self.terminos} 
                 """
 
 
 # Inicializando un objeto de la clase
-persona1 = Persona("César", "Varela", 43)
+persona1 = Persona("César", "Varela", 43, "Av. Oyamel", 250, m="manzana", p="pera")
 persona2 = Persona("Verónica", "Cortez", 43)
 persona3 = Persona("Vanessa", "Varela Corrales", 15)
 
