504df229821f5ee1a92e03776e57223feb4cdc6c
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-09 11:03:35 -0600

    Funciones: paso de diferentes tipos de datos como argumentos

---
+++ 06-funciones/6-7-fun-diferentes-pasos-argumentos.py
@@ -0,0 +1,18 @@
+def desplegar_nombres(nombres):
+    for nombre in nombres:
+        print(nombre)
+
+
+nombres = ["Juan", "Karla", "Guillermo"]
+
+
+desplegar_nombres(nombres)
+
+desplegar_nombres("Carlos")
+
+print("\n")
+desplegar_nombres({10, 3})
+print("\n")
+desplegar_nombres([7, 3])
+print("\n")
+desplegar_nombres((1, 2))
