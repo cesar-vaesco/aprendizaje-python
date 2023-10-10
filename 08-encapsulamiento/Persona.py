da3a15fa58e9f04a55a3a44c4f2b237bd92dd1ea
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-09 22:02:17 -0600

    Encapsulamiento: métodos getters y setters

--- 08-encapsulamiento/Persona.py
+++ 08-encapsulamiento/Persona.py
@@ -5,11 +5,40 @@ es inicializar los atributos del objeto que creamos.
 
 # Encapsular el valor de los atributos de la clase
 
+
 class Persona:
     # Atribuitos de clase
     def __init__(self, nombre, apellido, edad):
         self._nombre = nombre
         self._apellido = apellido
+        self._edad = edad
+
+    #  Declaración de métodos getters
+    @property
+    def nombre(self):
+        print("Llamando el método get")
+        return self._nombre
+
+    @property
+    def apellido(self):
+        return self._apellido
+
+    @property
+    def edad(self):
+        return self._edad
+
+    # Declaración de métodos setter
+    @nombre.setter
+    def nombre(self, nombre):
+        print("Llamando el método set")
+        self._nombre = nombre
+
+    @apellido.setter
+    def apellido(self, apellido):
+        self._apellido = apellido
+
+    @edad.setter
+    def edad(self, edad):
         self._edad = edad
 
     # la palabra self pasa la referencia de los valores asignados a los atributos de la clase
@@ -20,45 +49,18 @@ class Persona:
                     - Edad: {self._edad}
                 """
 
+
+# --> Fin del ambito de la clase Persona
+
 persona1 = Persona("Verónica", "Cortez", 43)
 persona2 = Persona("Vanessa", "Varela Corrales", 15)
 
-# persona2._nombre = "Carla"
-persona2.nombre = "Carla" 
+# print(persona1.nombre())
 
-print(persona1.mostrar_detalle())
-print(persona2.mostrar_detalle())
-
-
+print(persona1.nombre)
+persona1.nombre = "Veróniquilla"
+print(persona1.nombre)
 
 
-
-
-"""
-# Inicializando un objeto de la clase
-# persona1 = Persona("César", "Varela", 43)
-# persona2 = Persona("Verónica", "Cortez", 43)
-# persona3 = Persona("Vanessa", "Varela Corrales", 15)
-
-
-# print(
-#     f"""
-#     --- Objeto 1 ---
-#     Nombre: {persona1.nombre}
-#     Apellido: {persona1.apellido}
-#     Edad: {persona1.edad}
-#     """
-# )
-
-
-# print("\nCambiando los valores del objeto 1")
-
-# persona1.nombre = "Gloria"
-# persona1.apellido = "Varela Corrales"
-# persona1.edad = 22
-# persona1.telefono = "556581111"
-
-# Mandando llamar el méto mostrar_detalle usando
-# la referencia de un objeto a traves la llamada de la clase
-# -->  print(Persona.mostrar_detalle(persona3))
-# """
+# print(persona1.mostrar_detalle())
+# print(persona2.mostrar_detalle())
