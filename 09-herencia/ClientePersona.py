9517e9c0fff668463e749bf586f2b77c61d7e10d
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-10 09:42:30 -0600

    Herencia: método str --> impresión de los valores del objeto

---
+++ 09-herencia/ClientePersona.py
@@ -0,0 +1,9 @@
+from Persona import *
+
+
+persona1 = Persona("Facundo", 23)
+
+empleado1 = Empleado("José", 21, 5000)
+
+print(persona1)
+print(empleado1)
--- 09-herencia/Persona.py
+++ 09-herencia/Persona.py
@@ -3,6 +3,9 @@ class Persona:
         self.nombre = nombre
         self.edad = edad
 
+    def __str__(self):
+        return f"Persona: [Nombre: {self.nombre} - Edad: {self.edad}]"
+
 
 class Empleado(Persona):
     def __init__(self, nombre, edad, sueldo):
@@ -10,14 +13,17 @@ class Empleado(Persona):
         super().__init__(nombre, edad)
         self.sueldo = sueldo
 
+    def __str__(self):
+        return f"Empleado: [Sueldo: {self.sueldo}] {super().__str__()} "
 
-empleado1 = Empleado("José", "Juaréz", 5000)
+if __name__ == "__main__":
+    empleado1 = Empleado("José", "Juaréz", 5000)
 
-print("")
-print("Información de empleado".center(30, "*"))
-print("")
+    print("")
+    print("Información de empleado".center(30, "*"))
+    print("")
 
-print(f"Empleado: {empleado1.nombre}")
-print(f"Edad: {empleado1.edad}")
-print(f"Sueldo: {empleado1.sueldo}")
-print("")
+    print(f"Empleado: {empleado1.nombre}")
+    print(f"Edad: {empleado1.edad}")
+    print(f"Sueldo: {empleado1.sueldo}")
+    print("")
