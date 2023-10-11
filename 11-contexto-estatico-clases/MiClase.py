8c21c2810054d4493fc0d74222c71566f8e4905c
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-11 14:32:03 -0600

    Contexto estático: contexto estático y dinámico

--- 11-contexto-estatico-clases/MiClase.py
+++ 11-contexto-estatico-clases/MiClase.py
@@ -6,13 +6,17 @@ class MiClase:
     # Los métodos de clase pueden usar información de la clase -recibe referencias de la clase
     @classmethod
     def metodo_clase(cls):
-        return f"Método de clase: {cls.variable_clase}"
+        print( f"Método de clase: {cls.variable_clase}")
 
     # Método estatico -> se asocian a la clase pero no recibe
     # información de la clase (no se puede usar atributos de la clase), no hay referencias de la clase
     @staticmethod
     def metodo_estatico():
-        return f"Llamada de método estático(de clase): {MiClase.variable_clase}"
+        print( f"Llamada de método estático(de clase): {MiClase.variable_clase}")
+    
+    def metodo_instancia(self):
+        self.metodo_clase()
+        self.metodo_estatico()
 
     def __init__(self, variable_instancia):
         self.variable_instancia = variable_instancia
@@ -30,13 +34,15 @@ mi_clase2 = MiClase("Segunda instancia")
 
 MiClase.variable_clase2 = "Variable clase 2"
 
-print("\nInstancia de clase ".center(50, "*"))
+miObjeto1 = MiClase("Variable de instancia")
+
+print("1.-Instancia de clase ".center(50, "*"))
 
 print(f"Instancia de clase: {mi_clase.variable_instancia}")
 # Un objeto puede acceder a una variable de clase
 print(f"Variable de clase usada por una instancia: {mi_clase.variable_clase}")
 
-print("\nVariable de clase ".center(50, "*"))
+print("2.-Variable de clase ".center(50, "*"))
 print(f"Instancias de clase: {MiClase.contador}")
 
 print(
@@ -44,9 +50,17 @@ print(
 )
 print(f"Variable clase de clase 2 accediendo desde la clase: {MiClase.variable_clase2}")
 
-print("\nMétodo de clase de contexto estático ".center(50, "*"))
+print("\n3.-Método de clase de contexto estático ".center(50, "*"))
 
-print(f"Método de clase --> {MiClase.metodo_estatico()}")
+MiClase.metodo_estatico()
 
-print("\nMandando llamar método de clase ".center(50, "-"))
-print(MiClase.metodo_clase())
+print("4.-Mandando llamar método de clase ".center(50, "-"))
+MiClase.metodo_clase()
+
+print("5.-Accediendo a un método de clase ".center(50,"_"))
+
+print(f"Objeto de clase accediendo a método de clase: ")
+miObjeto1.metodo_clase()
+print(f"Instancia de clase(objeto): ")
+miObjeto1.metodo_estatico()
+miObjeto1.metodo_instancia()
