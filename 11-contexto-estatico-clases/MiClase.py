57aee8ca403fb30783d0fb56e77bff63daa4ac9b
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-11 11:43:36 -0600

    Contexto estático: variables de clase creada al vuelo(en tiempo de ejecución)

--- 11-contexto-estatico-clases/MiClase.py
+++ 11-contexto-estatico-clases/MiClase.py
@@ -13,8 +13,14 @@ class MiClase:
 # Instancia de clase
 mi_clase = MiClase("Valor variable de instancia")
 mi_clase2 = MiClase("Segunda instancia")
+
+MiClase.variable_clase2 = "Variable clase 2"
+
 print(f"Instancia de clase: {mi_clase.variable_instancia}")
 print(f"Instancia de clase: {mi_clase.variable_instancia}")
 # Un objeto puede acceder a una variable de clase
 print(f"Variable de clase usada por una instancia: {mi_clase.variable_clase}")
 print(f"Instancias de clase: {MiClase.contador}")
+
+print(f'Variable clase de clase 2 accediendo desde instancia: {mi_clase.variable_clase2}')
+print(f'Variable clase de clase 2 accediendo desde la clase: {MiClase.variable_clase2}')
