a725b8ef94d9c87899b9e203af51f3a71f5038ac
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-11 19:17:23 -0600

    Sobrecarga de operadores - ejemplo

---
+++ 13-sobrecarga_operadores/Persona.py
@@ -0,0 +1,18 @@
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def __add__(self, other):
+        return f"{self.nombre} {other.nombre}"
+
+    def __sub__(self, other):
+        return self.edad - other.edad
+
+
+persona1 = Persona("Juan", 40)
+persona2 = Persona("Carlos", 25)
+
+
+print(persona1 + persona2)
+print(persona1 - persona2)
