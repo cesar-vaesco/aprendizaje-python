e49f1a79536a909bf7d213b48c42bdfe72b437f6
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-16 17:39:43 -0600

    LaboratorioPC: Clase Orden y clase de test para probar el laboratorio

---
+++ 15-Laboratorio-PC/test_mundoPC.py
@@ -0,0 +1,30 @@
+from Computadora import Computadora
+from Monitor import Monitor
+from Raton import Raton
+from Teclado import Teclado
+from Orden import Orden
+
+teclado1 = Teclado('HP', 'USB')
+raton1 = Raton('HP', 'USB')
+monitor1 = Monitor('HP', 15)
+computadora1 = Computadora('HP', monitor1, teclado1, raton1)
+
+teclado2 = Teclado('Acer', 'Bluetooth')
+raton2 = Raton('Acer', 'Bluetooth')
+monitor2 = Monitor('Acer', 27)
+computadora2 = Computadora('Acer', monitor2, teclado2, raton2)
+
+teclado3 = Teclado('Gamer', 'Bluetooth')
+raton3 = Raton('Gamer', 'Bluetooth')
+monitor3 = Monitor('Gamer', '34')
+computadora3 = Computadora('Gamer', monitor3, teclado3, raton3)
+
+computadoras1 = [computadora1, computadora2]
+orden1 = Orden(computadoras1)
+# print(orden1)
+orden1.agregar_computadora(computadora3)
+print(orden1)
+
+computadoras2 = [computadora2, computadora3]
+orden2 = Orden(computadoras2)
+print(orden2)
