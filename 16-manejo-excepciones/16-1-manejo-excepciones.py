fec6711bd426521803a6561dba7cf901ddd81518
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-16 18:28:48 -0600

    Excepciones:jerarquía en el llamado de excepciones

--- 16-manejo-excepciones/16-1-manejo-excepciones.py
+++ 16-manejo-excepciones/16-1-manejo-excepciones.py
@@ -1,17 +1,20 @@
 resultado = None
-a = "10"
-b = 0
+
 
 try:
+    a = int(input("Primer número: "))
+    b = int(input("Segundo número: "))
     resultado = a / b
 
+# La clase de menor jerarquía de excepciones van al inicio de la llamada de las excepciones
+except ValueError as ve:
+    print(f"\nOcurrió un error: {ve} - {type(ve)}")
+except ZeroDivisionError as zde:
+    print(f"\nOcurrió un error: {zde} - {type(zde)}")
+except TypeError as te:
+    print(f"\nOcurrió un error: {te} - {type(te)}")
 except Exception as e:
-    print(f"\nOcurrió un error: {e}")
-
-# except ZeroDivisionError as e:
-#     print(f"\nOcurrió un error: {e}")
-# except TypeError as e:
-#     print(f"\nOcurrió un error: {e}")
+    print(f"\nOcurrió un error: {e} - {type(e)}")
 
 print(f"\nResultado: {resultado}")
 print("Continuamos...\n")
