77adeca0def6791bed96509a33280b8e558bd428
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-16 18:20:30 -0600

    Excepciones: conociendo el procesamiento del manejo de errores

--- 16-manejo-excepciones/16-1-manejo-excepciones.py
+++ 16-manejo-excepciones/16-1-manejo-excepciones.py
@@ -1,4 +1,17 @@
+resultado = None
+a = "10"
+b = 0
+
 try:
-    10 / 0
+    resultado = a / b
+
 except Exception as e:
-    print(f"\nOcurrió un error: {e}\n")
+    print(f"\nOcurrió un error: {e}")
+
+# except ZeroDivisionError as e:
+#     print(f"\nOcurrió un error: {e}")
+# except TypeError as e:
+#     print(f"\nOcurrió un error: {e}")
+
+print(f"\nResultado: {resultado}")
+print("Continuamos...\n")
