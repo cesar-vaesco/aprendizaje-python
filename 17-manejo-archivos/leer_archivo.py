cfd4a4218719abc4de288f49d02780b51fe72bb0
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-17 11:16:37 -0600

    Manejo-Archivos: copiar archivos

--- 17-manejo-archivos/leer_archivo.py
+++ 17-manejo-archivos/leer_archivo.py
@@ -5,13 +5,28 @@ try:
     # Recuperando caracteres de un archivo --> cantidad
     # print(archivo.read(10))
     # Leer línea completa
-    print(archivo.readline())
-    print(archivo.readline())
+    # print(archivo.readline())
+    # print(archivo.readline())
     # print(archivo.read())
+
+    # for linea in archivo:
+    #     print(linea)
+
+    # Leer lineas
+    # print(archivo.readlines())
+
+    # Acceder a una linea de la lista
+    # print(archivo.readlines()[2])
+
+    # Abrimos un nuevo archivo
+    # a - anexar información
+    archivo2 = open("17-manejo-archivos/copia.txt", "w", encoding="utf8")
+    archivo2.write(archivo.read())
 
 except Exception as e:
     print(e)
 
 finally:
     archivo.close()
+    archivo2.close()
     print("Cerrando archivo")
