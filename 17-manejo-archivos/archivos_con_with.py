73d5afccaf8a34071104af673b46a586cebd7057
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-17 11:33:22 -0600

    Manejo-Archivos-with: context-manager -> administrador de recursos

--- 17-manejo-archivos/archivos_con_with.py
+++ 17-manejo-archivos/archivos_con_with.py
@@ -1,2 +1,6 @@
-with open("17-manejo-archivos/prueba.txt", "r", encoding="utf8") as archivo:
+from manejo_archivos import ManejoArchivos
+
+
+# with open("17-manejo-archivos/prueba.txt", "r", encoding="utf8") as archivo:
+with ManejoArchivos("17-manejo-archivos/prueba.txt") as archivo:
     print(archivo.read())
