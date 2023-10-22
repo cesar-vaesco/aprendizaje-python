baa947dd95e74a779ef3b5e78688e7506ff1709d
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-22 15:38:11 -0600

    Databases: conexión base de datos con with

--- 19-databases/prueba_db.py
+++ 19-databases/prueba_db.py
@@ -3,7 +3,7 @@ import psycopg2
 
 # Base de datos creada perviamente en postgrest
 conexion = psycopg2.connect(
-    user = "postgres", 
+    user = "postgres",
     password = "admin",
     host = "127.0.0.1",
     port = "5432",
@@ -12,14 +12,18 @@ conexion = psycopg2.connect(
 
 # print(conexion)
 
-# Se crea un cursor para poder  usar sentencias sql 
+try:
 
-cursor = conexion.cursor()
-sentencia = 'SELECT * FROM persona'
-cursor.execute(sentencia)
-registros = cursor.fetchall()
+    with conexion:
+        # Se crea un cursor para poder  usar sentencias sql
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT * FROM persona'
+            cursor.execute(sentencia)
+            registros = cursor.fetchall()
 
-print(f"Registros: \n{registros}")
-
-cursor.close()
-conexion.close()
+            print(f"Registros: \n{registros}")
+except Exception as e:
+    print(f"Ocurrio un error: {e}")
+finally:
+    print(f"\nCerrando conexion....\n")
+    conexion.close()
