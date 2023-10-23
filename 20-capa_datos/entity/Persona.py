73f1df670a8a76ef3084377ea47c81e5dfc77d49
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-23 14:54:42 -0600

    CapaDatos: probando creación de objetos de la clase Persona

--- 20-capa_datos/entity/Persona.py
+++ 20-capa_datos/entity/Persona.py
@@ -1,5 +1,8 @@
+from logger_base import log
+
+
 class Persona:
-    def __init__ (self,id_persona, nombre, apellido, email):
+    def __init__ (self,id_persona=None, nombre=None, apellido=None, email=None):
         self._id_persona = id_persona
         self._nombre = nombre
         self._apellido = apellido
@@ -7,10 +10,10 @@ class Persona:
         
     def __str__ (self):
         return f'''
-        - Id Persona: {self._id_persona}
-        - Nombre: {self._nombre}
-        - Apellido: {self._apellido}
-        - Email: {self._email}
+- Id Persona: {self._id_persona}
+- Nombre: {self._nombre}
+- Apellido: {self._apellido}
+- Email: {self._email}
     '''
     
     @property
@@ -49,9 +52,15 @@ class Persona:
 
 
 if __name__ == "__main__":
+
+    persona1 = Persona(1, "José", "Róbles","jrobles@correo.com")
+    log.debug(persona1)
+
+    # Simular un insert
+    persona2 = Persona(nombre="José", apellido="Róbles", email="jrobles@correo.com")
+    log.debug(persona2)
+
+    # Simular la eliminación de un registro
+    persona3 = Persona(id_persona = 1)
+    log.debug(persona3)
     
-    persona1 = Persona(id_persona=1, nombre="José", apellido="Róbles", email="jrobles@correo.com")
-    
-    
-    
-    print(persona1)
