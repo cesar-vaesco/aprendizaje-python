9f90d7c1e29211c307545982c4ac4b6a49561aa1
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-24 13:20:10 -0600

    CapaDatos: actualizar con la clase PersonaDAO

--- 20-capa_datos/PersonaDao.py
+++ 20-capa_datos/PersonaDao.py
@@ -7,10 +7,11 @@ class PersonaDAO:
     """
     DAO = Data Access Object
     """
+
     _SELECCIONAR = 'SELECT * FROM persona ORDER BY id_persona'
     _INSERTAR = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
-    _ACTUALIZAR = 'INSERT INTO persona SET nombre = %s, apellido = %s, email = % WHERE id_persona = %s'
-    _ELIMINAR = 'DELETE FROM persona WHERE id_persona = %s'
+    _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+    _ELIMINAR = 'DELETE FROM persona WHERE id_persona=%s'
 
     @classmethod
     def seleccionar(cls):
@@ -21,10 +22,9 @@ class PersonaDAO:
             personas = []
 
             for registro in registros:
-                persona = Persona(registro[0], registro[1], registro[2],registro[3])
+                persona = Persona(registro[0], registro[1], registro[2], registro[3])
                 personas.append(persona)
             return personas
-
 
     @classmethod
     def insertar(cls, persona):
@@ -35,16 +35,37 @@ class PersonaDAO:
                 log.debug(f"Persona insertada: {persona}")
                 return cursor.rowcount
 
+    @classmethod
+    def actualizar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (
+                    persona.nombre,
+                    persona.apellido,
+                    persona.email,
+                    persona.id_persona,
+                )
+                cursor.execute(cls._ACTUALIZAR, valores)
+                log.debug(f'Persona actualizada: {persona}')
+                return cursor.rowcount
 
-if __name__ == '__main__':
 
-    # Insertar un registro
-    persona1 = Persona(nombre="Ro", apellido="Fg", email="rojo@correo.com")
-    personas_insertadas = PersonaDAO.insertar(persona1)
-    log.debug(f"Personas insertadas: {personas_insertadas}")
+if __name__ == "__main__":
+
+# Insertar un registro
+    # persona1 = Persona(nombre="Ricardo", apellido="Maldonado", email="rima@correo.com")
+    # personas_insertadas = PersonaDAO.insertar(persona1)
+    # log.debug(f"Personas insertadas: {personas_insertadas}")
+
+# Modificar registro
+    persona2 = Persona(68,"Lau", "Vega", "lave@correo.com")
+    print(persona2)
+    personas_actualizadas = PersonaDAO.actualizar(persona2)
+    log.debug(f"Personas actualizados: {personas_actualizadas}")
 
 
+# Listar registros
     personas = PersonaDAO.seleccionar()
-
     for persona in personas:
         log.info(persona)
+
