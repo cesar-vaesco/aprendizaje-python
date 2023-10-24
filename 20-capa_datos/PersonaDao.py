04d77beef5766e990d302a3e18db22b6713bcddb
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-24 13:38:57 -0600

    CapaDatos: eliminando registros  con la clase PersonaDAO

--- 20-capa_datos/PersonaDao.py
+++ 20-capa_datos/PersonaDao.py
@@ -49,6 +49,15 @@ class PersonaDAO:
                 log.debug(f'Persona actualizada: {persona}')
                 return cursor.rowcount
 
+    @classmethod
+    def eliminar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                # Espera una tupla por eso el id_persona es seguido por una coma
+                valores = (persona.id_persona,)
+                cursor.execute(cls._ELIMINAR, valores)
+                log.debug(f'Registro eliminado: {persona}')
+                return cursor.rowcount
 
 if __name__ == "__main__":
 
@@ -57,12 +66,16 @@ if __name__ == "__main__":
     # personas_insertadas = PersonaDAO.insertar(persona1)
     # log.debug(f"Personas insertadas: {personas_insertadas}")
 
-# Modificar registro
-    persona2 = Persona(68,"Lau", "Vega", "lave@correo.com")
-    print(persona2)
-    personas_actualizadas = PersonaDAO.actualizar(persona2)
-    log.debug(f"Personas actualizados: {personas_actualizadas}")
+# Actualizar registro
+    # persona2 = Persona(68,"Lau", "Vega", "lave@correo.com")
+    # personas_actualizadas = PersonaDAO.actualizar(persona2)
+    # log.debug(f"Personas actualizados: {personas_actualizadas}")
 
+
+# Eliminar registros
+    persona = Persona(id_persona=56)
+    personas_eliminadas = PersonaDAO.eliminar(persona)
+    log.debug(f"Eliminadas: {personas_eliminadas}")
 
 # Listar registros
     personas = PersonaDAO.seleccionar()
