e9c977ff02c66d5901e9e5b2755954e376ee643d
César Vargas <cesarvargasescorcia@gmail.com>
2023-10-25 16:53:06 -0600

    Pool-Conexiones: probando la clase CursorDelPool

--- 21-pool-conexiones/21-pool-conexiones/cursor_del_pool.py
+++ 21-pool-conexiones/21-pool-conexiones/cursor_del_pool.py
@@ -26,3 +26,10 @@ class CursorDelPool:
         self._cursor.close()
         
         Conexion.liberarConexion(self._conexion)
+        
+        
+if __name__ == "__main__":
+    with CursorDelPool() as cursor:
+        log.debug("Dentro del bloque with")
+        cursor.execute("SELECT * FROM persona")
+        log.debug(cursor.fetchall())
