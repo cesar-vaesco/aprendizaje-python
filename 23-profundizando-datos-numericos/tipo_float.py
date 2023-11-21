a053b01829ddad558b9196b3321c0e1461da822e
César Vargas <cesarvargasescorcia@gmail.com>
2023-11-21 08:16:01 -0600

    Profundizando: Más sobre tipo float

---
+++ 23-profundizando-datos-numericos/tipo_float.py
@@ -0,0 +1,22 @@
+# Profundizando tipo float
+
+a = 3
+# print(f"a: {a:.5f}")
+
+# Constructor tipo float puede recibir datos
+# tipo int y string
+
+a = float(10)
+a = float("23")
+# print(f"a: {a:.2f}")
+
+
+# Notación exponencial (Valores positivos o negativos)
+
+a = 3e5
+a = 3e-5
+# print(f"a: {a:.5f}")
+
+# Cualquier cálculo que involucre float, se convierte a float
+a = 4.0 + 5
+print(a)
