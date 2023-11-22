c9f0a957d0a59720998a933695bda971fec436ac
César Vargas <cesarvargasescorcia@gmail.com>
2023-11-22 09:39:09 -0600

    Profundizando: str --> docsstrings

---
+++ 24-profundizando-strings/mi_clase.py
@@ -0,0 +1,20 @@
+class MiClase:
+
+    """
+    Este es un ejemplo de la documentación 
+    de nuestra clase
+    """
+
+    def __init__(self):
+        """
+        Método de inicio 
+        de nuestra clase
+        """
+
+    def mi_metodo(self, param1, param2):
+        '''
+        Esta es la documentación de nuestro método
+        :param1: es el parametro1
+        :param2: es el parametro2
+        return: esel retorno de la función
+        '''
--- 24-profundizando-strings/tipo_str.py
+++ 24-profundizando-strings/tipo_str.py
@@ -1,8 +1,25 @@
+import math
 
+from mi_clase import MiClase
 # Concatenación automatica en python
+# variable = "Adios"
+# mensaje = "Hola"  " Mundo " +  variable
+# mensaje += " Universidad" " Python" 
 
-variable = "Adios"
-mensaje = "Hola"  " Mundo " +  variable
-mensaje += " Universidad" " Python" 
+# print(mensaje)
 
-print(mensaje)
+# help(math)
+
+# help(str)
+
+
+"""
+Comentarios varías líneas
+"""
+
+# help(MiClase)
+print(MiClase.__doc__)
+print(MiClase.__init__.__doc__)
+print(MiClase.mi_metodo.__doc__)
+print(MiClase.mi_metodo)
+print(type(MiClase.mi_metodo))
