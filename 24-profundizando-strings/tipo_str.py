8bb18f3ad74c0edf2a971e4d44ac1538bf557710
César Vargas <cesarvargasescorcia@gmail.com>
2023-11-22 09:52:06 -0600

    Profundizando: str --> mutabilidad

--- 24-profundizando-strings/tipo_str.py
+++ 24-profundizando-strings/tipo_str.py
@@ -1,10 +1,11 @@
 import math
 
 from mi_clase import MiClase
+
 # Concatenación automatica en python
 # variable = "Adios"
 # mensaje = "Hola"  " Mundo " +  variable
-# mensaje += " Universidad" " Python" 
+# mensaje += " Universidad" " Python"
 
 # print(mensaje)
 
@@ -18,8 +19,23 @@ Comentarios varías líneas
 """
 
 # help(MiClase)
-print(MiClase.__doc__)
-print(MiClase.__init__.__doc__)
-print(MiClase.mi_metodo.__doc__)
-print(MiClase.mi_metodo)
-print(type(MiClase.mi_metodo))
+# print(MiClase.__doc__)
+# print(MiClase.__init__.__doc__)
+# print(MiClase.mi_metodo.__doc__)
+# print(MiClase.mi_metodo)
+# print(type(MiClase.mi_metodo))
+
+help(str.capitalize)
+
+mensaje1 = "hola mundo"
+# No muta el valor de la variable mensaje1
+mensaje2 = mensaje1.capitalize()
+
+print(f"mensaje1: {mensaje1}, id: {hex(id(mensaje1))}")
+print(f"mensaje2: {mensaje2}, id: {hex(id(mensaje2))}")
+
+# se modifica la variable
+mensaje1 = "adios"
+print(f"mensaje1: {mensaje1}, id: {hex(id(mensaje1))}")
+
+
