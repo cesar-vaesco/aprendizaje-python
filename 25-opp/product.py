7ebf913bd347b2979a716bd552fd2ffd64cf74d9
César Vargas <cesarvargasescorcia@gmail.com>
2023-11-26 14:27:48 -0600

    OOP: deleter

--- 25-opp/product.py
+++ 25-opp/product.py
@@ -7,33 +7,63 @@ class Product:
         return f"x: {self._x} - y: {self._y}"
 
     @property
-    def get_x(self):
+    def value(self):
         return self._x
 
-    @get_x.setter
-    def set_x(self, x):
-        self._x = x
+    @value.setter
+    def value(self, val):
+        self._x = val
 
-    @property
-    def get_y(self):
-        return self._y
-
-    @get_y.setter
-    def set_y(self, y):
-        self._y = y
+    @value.deleter
+    def value(self):
+        del p1.value
+        return "Value deleter"
 
 
-p = Product(5, 10)
+p1 = Product(5, 10)
 
-print(p.display())
-print(f"Valor actual de x: {p.get_x}")
-p.set_x = 15
-print(f"Valor actual de x: {p.get_x}")
-print(f"Valor actual de y: {p.get_y}")
-p.set_y = 65
-print(f"Valor actual de y: {p.get_y}")
-print(p.display())
+print(p1.value)
 
+
+
+print(f"{p1.value}")
+
+
+# class Product:
+#     def __init__(self, x, y) -> None:
+#         self._x = x
+#         self._y = y
+
+#     def display(self):
+#         return f"x: {self._x} - y: {self._y}"
+
+#     @property
+#     def get_x(self):
+#         return self._x
+
+#     @get_x.setter
+#     def set_x(self, x):
+#         self._x = x
+
+#     @property
+#     def get_y(self):
+#         return self._y
+
+#     @get_y.setter
+#     def set_y(self, y):
+#         self._y = y
+
+
+# p = Product(5, 10)
+
+# print(p.display())
+# print(f"Valor actual de x: {p.get_x}")
+# p.set_x = 15
+# print(f"Valor actual de x: {p.get_x}")
+# print(f"Valor actual de y: {p.get_y}")
+# p.set_y = 65
+# print(f"Valor actual de y: {p.get_y}")
+# print(p.display())
 
 
 #     def __init__(self):
