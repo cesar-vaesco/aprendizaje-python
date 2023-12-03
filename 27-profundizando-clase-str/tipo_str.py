21d4178f03b561bfcfb9e3e0538ddc531b76e858
César Vargas <cesarvargasescorcia@gmail.com>
2023-12-03 13:30:24 -0600

    27-profundizando-clase-str: multiplicación de cadenas

---
+++ 27-profundizando-clase-str/tipo_str.py
@@ -0,0 +1,20 @@
+resultado = 5 * "Hola "
+print(f"Resultado: {resultado}")
+
+
+# Multiplicacion de tuplas
+
+resultado = 5 * ("Hola",)
+print(f"Resultado: {resultado}")
+
+
+resultado = 5 * ("Hola", "Mundo")
+print(f"Resultado: {resultado}")
+
+resultado = 5 * ("Hola", 10)
+print(f"Resultado: {resultado}")
+
+# Multiplicación de listas
+resultado = 10 * [0]
+print(f"Resultado: {resultado}, largo: {len(resultado)}")
+
