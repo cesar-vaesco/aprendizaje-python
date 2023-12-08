b1044deea5741f080d96c6af030bae1382b9908d
César Vargas <cesarvargasescorcia@gmail.com>
2023-12-07 20:31:03 -0600

    27-profundizando clase string: caracteres ascii

---
+++ 27-profundizando-clase-str/set_caracteres_ascii.py
@@ -0,0 +1,12 @@
+""" 
+https://elcodigoascii.com.ar/
+"""
+
+caracter = chr(65)
+print(f"caracter A: {caracter}")
+
+caracter = chr(64)
+print(f"caracter @: {caracter}")
+
+caracter = chr(97)
+print(f"caracter a: {caracter}")
