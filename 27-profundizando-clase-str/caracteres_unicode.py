6ccd1c3d0ce3402bf83639a032490bc1dfe497fa
César Vargas <cesarvargasescorcia@gmail.com>
2023-12-07 20:15:19 -0600

    27-profundizando clase string: caracteres unicode

---
+++ 27-profundizando-clase-str/caracteres_unicode.py
@@ -0,0 +1,9 @@
+"""
+Tabla caracteres caractres Unicode
+https://symbl.cc/es/unicode/table/#basic-latin
+"""
+
+print("Hola\u0020mundo")
+print("Notación simple: \u0041")
+print("Notación extendida: \U00000041")
+print("Notación hexadecimal: \x41")
