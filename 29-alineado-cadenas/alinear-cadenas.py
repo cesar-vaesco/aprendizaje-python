6fa628a339b59911372dec4f6895aa04b71fc209
César Vargas <cesarvargasescorcia@gmail.com>
2023-12-10 18:40:10 -0600

    29-alinear-cadenas: alinear cadenas a la izquierda y a la derecha

--- 29-alineado-cadenas/alinear-cadenas.py
+++ 29-alineado-cadenas/alinear-cadenas.py
@@ -9,3 +9,13 @@ print(titulo.center(50, "*"))
 print("Caracteres de la caderna titulo: ", len(titulo.center(50, "#")))
 
 print(titulo.center(len(titulo) + 10, "-"))
+
+print("alineando cadenas a la izquierda".center(100, "<"))
+
+print(titulo.ljust(50, "-"))
+print(titulo.ljust(len(titulo) + 10, "-"))
+
+print("alineando cadenas a la derecha".center(100, ">"))
+
+print(titulo.rjust(50, ">"))
+print(titulo.rjust(len(titulo) + 10, ">"))
