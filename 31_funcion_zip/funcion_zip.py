8a246b7e4f5191b795a38377a8c23de77316e033
César Vargas <cesarvargasescorcia@gmail.com>
2023-12-17 17:57:23 -0600

    31-funciónZIP: iterar varios iterables de forma simultanea con función zip

--- 31_funcion_zip/funcion_zip.py
+++ 31_funcion_zip/funcion_zip.py
@@ -21,10 +21,34 @@ mezcla_tupla = zip(numeros_tupla, letras)
 # print(f"Ubicación en memoria de la mezcla: {mezcla}")
 # Converir a lista para poder listar los elementos
 mezcla = list(mezcla)
-mezcla_tupla = tuple(mezcla_tupla)
+# mezcla_tupla = tuple(mezcla_tupla)
 
 print(f"\nSe crea una lista de tuplas: {mezcla}\n")
 # print(f"Se crea una tupla de tuplas: {mezcla_tupla}")
 
 # print(type(mezcla))
 # print(type(mezcla_tupla))
+
+print("\nIterar en paralelo")
+
+for numero, letra, identificador, aleatorio in zip(
+    numeros, letras, identificadores, conjunto
+):
+    print(
+        f"""
+- Número: {numero} 
+- Letra: {letra} 
+- Identificador: {identificador} 
+- Aleatorio: {aleatorio}"""
+    )
+
+
+nueva_lista = []
+
+for numero, letra, identificador, aleatorio in zip(
+    numeros, letras, identificadores, conjunto
+):
+    nueva_lista.append(f"{identificador} - {numero} - {letra} - {aleatorio}")
+
+print("\nCrando una nueva lista mezclando elementos de varios iterables:\n")
+print("\t", nueva_lista)
