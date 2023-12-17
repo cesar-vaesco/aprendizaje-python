52f25fdd2d023eaa99fd437150c66d87165001fd
César Vargas <cesarvargasescorcia@gmail.com>
2023-12-17 15:53:56 -0600

    30-desempaquetado: operador unpacking - ejemplo de uso

--- 30-desempaquetado/operador_unpacking.py
+++ 30-desempaquetado/operador_unpacking.py
@@ -3,3 +3,11 @@ print(numeros)
 
 print(*numeros)
 print(*numeros, sep=" - ")
+
+
+#  Desempaquetando al momento de parar un parámetro a una función
+def sumar(a, b, c):
+    return f"Resultado suma: {a + b + c}"
+
+
+print(sumar(*numeros))
