1f7d2f908a0165f40f97f7e40e14f7ce701f741a
César Vargas <cesarvargasescorcia@gmail.com>
2023-12-17 17:47:58 -0600

    31-funciónZIP: probando unión con iterables set, tuplas y listas

--- 31_funcion_zip/funcion_zip.py
+++ 31_funcion_zip/funcion_zip.py
@@ -3,16 +3,28 @@
 
 numeros = [1, 2, 3]
 numeros_tupla = (1, 2, 3)
-letras = ["a", "b", "c"]
-mezcla = zip(numeros, letras)
+letras = ["a", "b", "c", "d"]
+identificadores = (
+    321,
+    322,
+    323,
+    324,
+)
+conjunto = {6, 4, 0, 9, 8, 15, 10}
+mezcla = zip(numeros_tupla, letras, identificadores, conjunto)
 mezcla_tupla = zip(numeros_tupla, letras)
 
-print(type(mezcla))
 
-print(f"Ubicación en memoria de la mezcla: {mezcla}")
+# print(type(mezcla))
+# print(type(mezcla_tupla))
+
+# print(f"Ubicación en memoria de la mezcla: {mezcla}")
 # Converir a lista para poder listar los elementos
 mezcla = list(mezcla)
 mezcla_tupla = tuple(mezcla_tupla)
 
-print(f"Se crea una lista de tuplas: {mezcla}")
-print(f"Se crea una tupla de tuplas: {mezcla_tupla}")
+print(f"\nSe crea una lista de tuplas: {mezcla}\n")
+# print(f"Se crea una tupla de tuplas: {mezcla_tupla}")
+
+# print(type(mezcla))
+# print(type(mezcla_tupla))
