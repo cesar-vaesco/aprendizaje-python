0518dd8e3b418f8cfdcd837d19a92ec918376ca2
César Vargas <cesarvargasescorcia@gmail.com>
2023-12-19 06:56:19 -0600

    06-funciones lambda: ejemplos de uso de una función lambda

--- 06-funciones/6-17-funciones-lambda.py
+++ 06-funciones/6-17-funciones-lambda.py
@@ -13,3 +13,33 @@ resultado = mi_funcion_lambda(4, 6)
 
 print(f"resultado: {resultado}")
 print(f"resultado: {mi_funcion_lambda(5, 7)}")
+
+# Utilizar función lambda que no recibe argumentos
+mi_funcion_lambda = lambda: "Mi función lambda sin argumentos"
+resultado = mi_funcion_lambda()
+print(f"Llamar función lambda sin argumentos: {mi_funcion_lambda}")
+print(f"Llamar función lambda sin argumentos: {resultado}")
+
+
+#  Función lambda con parámetros por default
+mi_funcion_lambda = lambda a=2, b=3: a + b
+resultado = mi_funcion_lambda()
+print(f"Resultado argumentos por default: {resultado}")
+
+resultado = mi_funcion_lambda(6, 12)
+print(f"Resultado: {resultado}")
+
+
+# Función lambda con argumentos variables utilizando *args - **kwargs
+mi_funcion_lambda = lambda *args, **kwargs: len(args) + len(kwargs)
+print(f"Resultado argumentos variables: {mi_funcion_lambda(2,4,6, a=5, b=6 )}")
+
+
+# Funciones lambda con argumentos, argumentos variables y variables por default
+mi_funcion_lambda = (
+    lambda a, b, c=3, *args, **kwargs: a + b + c + len(args) + len(kwargs)
+)
+
+print(
+    f"Resultado argumentos variables: {mi_funcion_lambda(1, 2, 4, 5, 6,7, e=5, f= 6)}"
+)
