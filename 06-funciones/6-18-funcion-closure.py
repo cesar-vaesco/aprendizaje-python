15bd0cfbc6c6a6af2ae4bfa154c1aef919d85a8f
César Vargas <cesarvargasescorcia@gmail.com>
2023-12-19 07:13:17 -0600

    06-funciones lambda: closure y funciones lambda

--- 06-funciones/6-18-funcion-closure.py
+++ 06-funciones/6-18-funcion-closure.py
@@ -19,3 +19,25 @@ print(f"Resultado de la función closure: {mi_funcion_closure()}")
 
 # Llamar la función regresada al vuelo (en tiempo de interpretación)
 print(f"Resultado de la función closure al vuelo: {operacion(5,6)()}")
+
+"""
+def operacion(a, b):
+    # Definimos una función interna o anidada
+    def sumar():
+        return a + b
+
+    # Retornar la función
+    return sumar
+
+"""
+
+
+# Función principal
+def operacion_closure_lambda(a, b):
+    # Función lambda interna o anidada
+    return lambda: a + b
+
+
+resultado = operacion_closure_lambda(5, 6)
+print(f"Resultado funcion closure_lambda: {resultado()}")
+print(f"Resultado funcion closure_lambda al vuelo: {operacion_closure_lambda(5,9)()}")
