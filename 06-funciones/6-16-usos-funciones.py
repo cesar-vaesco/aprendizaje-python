d3d2607a03aa7fdcf4cec44c707934cc34576505
César Vargas <cesarvargasescorcia@gmail.com>
2023-12-18 23:28:49 -0600

    06-funciones: funciones y sus distintos usos en python

--- 06-funciones/6-16-usos-funciones.py
+++ 06-funciones/6-16-usos-funciones.py
@@ -18,3 +18,21 @@ print(type(mi_funcion))
 # Llamamos la función a través de la
 resultado = mi_funcion(8, 5)
 print(f"Resultado: {resultado}")
+
+
+# 2.- Función como argumento
+def operacion(a, b, sumar_arg):
+    print(f"Resultado sumar: {sumar_arg(a, b)}")
+
+
+operacion(4, 5, sumar)
+
+
+# 3.- Podemos retornar una función
+def retornar_funcion():
+    # Retornamos una función
+    return sumar
+
+
+mi_funcion_retornada = retornar_funcion()
+print(f"Resultado función retornada: {mi_funcion_retornada(4, 7)}")
