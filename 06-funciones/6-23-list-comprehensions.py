8440020ef1be6f0b06e63c5931fd188122b4746c
César Vargas <cesarvargasescorcia@gmail.com>
2023-12-28 13:23:56 -0600

    06-List comprehensions -> uso de condiciones

--- 06-funciones/6-23-list-comprehensions.py
+++ 06-funciones/6-23-list-comprehensions.py
@@ -24,3 +24,43 @@ print("\n", " Mismo ejercicio pero con list-comprehensions ".center(60, "*"))
 lista_pares = [numero * numero for numero in numeros if numero % 2 == 0]
 print(f"Numeros pares de la lista: {lista_mostrar_pares}")
 print(f"[list comprehensions] Numeros pares múltiplicados por sí mismo: {lista_pares}")
+
+print(
+    "\n",
+    " Mismo ejercicio similar pero con dos condiciones pero con list-comprehensions ".center(
+        60, "*"
+    ),
+)
+print("\t Sólo se agrega el valor a la lista cuando el valor cumple ambas condiciones")
+
+pares = [numero for numero in range(50) if numero % 2 == 0 if numero % 6 == 0]
+print(f"Número divisible entre 2 y entre 6 de la lista: {pares}")
+
+print("\ncon uso de if-else")
+
+lista_pares = []
+lista_impares = []
+
+for numero in range(10):
+    if numero % 2 == 0:
+        lista_pares.append(numero)
+    else:
+        lista_impares.append(numero)
+
+print(f"Lista pares: {lista_pares}")
+print(f"Lista impares: {lista_impares}")
+
+
+print(
+    "\n[list-comprehensions] Sólo se agrega el valor a la lista cuando el valor cumple ambas condiciones"
+)
+lista_pares = []
+lista_impares = []
+
+[
+    lista_pares.append(numero) if numero % 2 == 0 else lista_impares.append(numero)
+    for numero in range(10)
+]
+
+print(f"[list-comprehensions] Lista pares: {lista_pares}")
+print(f"[list-comprehensions] Lista impares: {lista_impares}")
