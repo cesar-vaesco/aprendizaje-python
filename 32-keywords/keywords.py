9a61000baf97583152d707ae4cea106431ab1407
César Vargas <cesarvargasescorcia@gmail.com>
2023-12-28 13:45:52 -0600

    keywords en python

---
+++ 32-keywords/keywords.py
@@ -0,0 +1,9 @@
+import keyword
+
+print("Palabras reservadas (keywords) en Python")
+print(keyword.kwlist)
+
+# No se puede utilizar nungún keyword para nombrar una variable oi función
+# as = "Hola"
+# def is():
+#     pass
