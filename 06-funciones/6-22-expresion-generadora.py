88db91ed768b1d1b0de7516ed54f19174639059f
César Vargas <cesarvargasescorcia@gmail.com>
2023-12-28 12:52:03 -0600

    06-funciones tipo generadores: expresiones generadoras con listas

--- 06-funciones/6-22-expresion-generadora.py
+++ 06-funciones/6-22-expresion-generadora.py
@@ -20,7 +20,43 @@ except StopIteration:
 print("\n", " Pasando expresión generadora a una función...  ".center(60, "*"), "\n")
 
 suma = sum(valor * valor for valor in range(0, 4))
-print(f"Resultado: {suma}")
-print(f"Resultado: {suma}")
-print(f"Resultado: {suma}")
-print(f"Resultado: {suma}")
+print(f"Resultado suma: {suma}")
+print("\n", " Usando una lista...  ".center(60, "*"), "\n")
+
+
+lista = ["Juan", "Pérez"]
+
+generador = (valor for valor in lista)
+
+print(next(generador))
+print(next(generador))
+
+print(
+    "\n",
+    " Crear un string a partir de un generador creado a partir de una lista...  ".center(
+        80, "*"
+    ),
+    "\n",
+)
+
+
+lista = ["Karla", "Gómez", 22]
+print(f"Lista a modificar: {lista}")
+contador = 0
+
+
+def incrementar():
+    global contador
+    contador += 1
+    return contador
+
+
+#  Primera parte es el yield del generador, la segunda es el for entre parentesís
+generador = (f"{incrementar()}. {nombre}" for nombre in lista)
+
+lista = list(generador)
+print(lista)
+
+cadena = ", ".join(lista)
+print(f"Cadena modificada: {cadena}")
+print(f"Tipo de dato de la variable cadena: {type(cadena)}")
