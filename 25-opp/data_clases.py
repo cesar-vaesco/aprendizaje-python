8b0b00e6e799fc0ea7988c224cab742668ec47ca
César Vargas <cesarvargasescorcia@gmail.com>
2024-02-01 17:43:17 -0600

    dataclass: uso y enlace de dos clases con decorador @dataclass

--- 25-opp/data_clases.py
+++ 25-opp/data_clases.py
@@ -2,10 +2,17 @@ from dataclasses import dataclass
 from typing import ClassVar
 
 
-@dataclass
+@dataclass(eq=True, frozen=True)
+class Domicilio:
+    calle: str
+    numero: int = 0
+
+
+@dataclass(eq=True, frozen=True)
 class Persona:
     nombre: str
-    apellido: str = None
+    apellido: str
+    domicilio: Domicilio
     contador_personas: ClassVar[int] = 0
 
     def __post_init__(self):
@@ -15,7 +22,10 @@ class Persona:
             Persona.contador_personas += 1
 
 
-persona1 = Persona("Juan", "Peréz")
+domicilio1 = Domicilio("Marte", 15)
+domicilio2 = Domicilio("Mercurio", 105)
+
+persona1 = Persona("Juan", "Peréz", domicilio1)
 print(f"{persona1!r}")
 print(persona1)
 
@@ -24,6 +34,19 @@ print(f"Variable de clase: {Persona.contador_personas}")
 # Variables de instancia
 print(f"{persona1.__dict__}")
 #  Variable con valores vacíos
-persona_vacia = Persona("Joaquín", "")
+persona_vacia = Persona("Joaquín", "", domicilio2)
 print(f"Persona vacía: {persona_vacia}")
+print(f"Variable de clase: {Persona.contador_personas}")
+
+# Revisar igualdad entre objetos dataclass
+persona2 = Persona("Juan", "Peréz", domicilio1)
+print(f"Objetos iguales?:  {persona1 == persona2}")
+# Agregar esta clase a una colección
+coleccion = {persona1, persona2}
+# Parametro de @dataclass forzen = True
+# coleccion[0].nombre = "Juan Carlos"
+# print(coleccion)
+# persona1.nombre = "Juan Carlos"
+# print(coleccion)
+
 print(f"Variable de clase: {Persona.contador_personas}")
