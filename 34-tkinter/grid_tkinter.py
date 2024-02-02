0bed07a22ae8e023b73dabfc9ae74b5671a26d59
César Vargas <cesarvargasescorcia@gmail.com>
2024-02-02 13:31:43 -0600

    TKINTER: manejo de grid - posicionamiento de varios elementos dentro de la grid(cuadrilla)

--- 34-tkinter/grid_tkinter.py
+++ 34-tkinter/grid_tkinter.py
@@ -24,17 +24,38 @@ def evento2():
     boton2.config(text="Botón 2 presionado")
 
 
+def evento3():
+    boton3.config(text="Botón 3 presionado")
+
+
+def evento4():
+    boton4.config(text="Botón 4 presionado")
+
+
 ventana = tk.Tk()
 ventana.geometry("600x400")
 ventana.title("Manejo de Grid")
 ventana.iconbitmap("34-tkinter/icono.ico")
 
+# Configurar el grid
+ventana.rowconfigure(0, weight=2)
+ventana.rowconfigure(1, weight=10)
+ventana.columnconfigure(0, weight=1)
+ventana.columnconfigure(1, weight=5)
+
 boton1 = ttk.Button(ventana, text="Botón 1", command=evento1)
-boton1.grid(row=0, column=0, sticky="W")
+boton1.grid(row=0, column=0, sticky="NSWE")
 
 # N(arriba),E(derecha),S(abajo),w(izquierda)
 boton2 = ttk.Button(ventana, text="Botón 2", command=evento2)
-boton2.grid(row=1, column=0, sticky="E")
+boton2.grid(row=1, column=0, sticky="NSWE")
+
+boton3 = ttk.Button(ventana, text="Botón 3", command=evento3)
+boton3.grid(row=0, column=1, sticky="NSWE")
+
+# N(arriba),E(derecha),S(abajo),w(izquierda)
+boton4 = ttk.Button(ventana, text="Botón 4", command=evento4)
+boton4.grid(row=1, column=1, sticky="NSWE")
 
 
 center_window(ventana)
