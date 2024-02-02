51310b22a399de682102d95f8abd6ddf466b3b09
César Vargas <cesarvargasescorcia@gmail.com>
2024-02-02 14:40:07 -0600

    TKINTER: manejo de variables y componentes Entry

--- 34-tkinter/grid_entry.py
+++ 34-tkinter/grid_entry.py
@@ -17,7 +17,17 @@ def center_window(window):
 
 
 def enviar():
+    # Recuperamos la información a partir de la variable asociada con la caja de texto
+    boton1.config(text=entrada_var1.get())
+    # Modificación utilizamos la variable de texto
+    entrada_var1.set("Cambio...")
+    # Recuperamos la información
+    print(entrada_var1.get())
     print(entrada1.get())
+
+
+"""
+print(entrada1.get())
     boton1.config(text=entrada1.get())
     # Elimknar el contenido
     # entrada1.delete(0, tk.END)
@@ -26,6 +36,8 @@ def enviar():
     # Para hacer efectiva la selección
     entrada1.focus()
 
+"""
+
 
 ventana = tk.Tk()
 ventana.geometry("600x400")
@@ -33,15 +45,19 @@ ventana.title("Entry data tkinter")
 ventana.iconbitmap("34-tkinter/icono.ico")
 
 
+# Definimos una variable
+entrada_var1 = tk.StringVar(value="Valor por dafault")
+
 #  width es la cantidad de caracteres que ocupa la caja
 # entrada1 = ttk.Entry(ventana, width=30, justify=tk.CENTER, show="*")
-entrada1 = ttk.Entry(ventana, width=30, justify=tk.CENTER)
+entrada1 = ttk.Entry(ventana, width=30, textvariable=entrada_var1)
 entrada1.grid(row=0, column=0)
 #  Insert agrega un texto a nuestra caja de texto
 #  Texto de guía para introducir datos en la caja de texto
-entrada1.insert(0, "Introduce una cadena")
-entrada1.insert(tk.END, ":  ")
+# entrada1.insert(0, "Introduce una cadena")
+# entrada1.insert(tk.END, ":  ")
 # entrada1.config(state="readonly")
+
 
 # Creamos un botón
 boton1 = ttk.Button(ventana, text="Enviar", command=enviar)
